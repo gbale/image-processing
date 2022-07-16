dd189c7a94a6e90cae17e8489a9e66c866bb2556
Geoff <gbale@users.noreply.github.com>
2022-07-15 22:59:35 -0700

    Updated entry point

--- main.js
+++ main.js
@@ -7,7 +7,12 @@
  *
  */
 
-const IOhandler = require("./IOhandler"),
+const IOhandler = require('./IOhandler'),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then(IOhandler.readDir)
+  .then((filePaths) => filePaths.forEach((path) => IOhandler.grayScale(path, pathProcessed)))
+  .catch((err) => console.error(err));
